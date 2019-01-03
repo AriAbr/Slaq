f064878e1b2afedf7d40b9cac84a634af0b2b46a
Ari <ari.abramowitz1@gmail.com>
2019-01-03 14:29:17 -0500

    checkpoint 2765 create chat rooms: completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import * as firebase from 'firebase';
-import { Route } from 'react-router-dom';
+// import { Route } from 'react-router-dom';
 import RoomList from './components/RoomList';
 
 // Initialize Firebase
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -6,6 +6,7 @@ class RoomList extends Component {
     super(props);
     this.state = {
       rooms: [],
+      newRoomName: "",
     };
     this.roomsRef = this.props.firebase.database().ref('rooms');
   }
@@ -16,6 +17,20 @@ class RoomList extends Component {
       room.key = snapshot.key;
       this.setState({ rooms: this.state.rooms.concat( room ) });
     });
+  }
+
+  handleRoomSubmit(e) {
+    e.preventDefault();
+    if (!this.state.newRoomName) {return}
+    const newRoomName = this.state.newRoomName;
+    this.roomsRef.push({
+      name: newRoomName
+    });
+    this.setState({ newRoomName: '' });
+  }
+
+  handleRoomInputChange(e) {
+    this.setState({ newRoomName: e.target.value });
   }
 
   render() {
@@ -29,6 +44,21 @@ class RoomList extends Component {
         This is the RoomList component
         <h1 id="room-list-title">Bloc Chat</h1>
         {rooms}
+        <form onSubmit={ (e) => this.handleRoomSubmit(e) }>
+          <fieldset>
+            <legend>Create New Chat Room</legend>
+            <label>
+              Name:
+              <input
+                type="text"
+                id="new-room-input"
+                value={ this.state.newRoomName }
+                onChange={ (e) => this.handleRoomInputChange(e) }
+              />
+            </label>
+            <input type="submit" value="Submit" />
+          </fieldset>
+        </form>
       </div>
     );
   }
